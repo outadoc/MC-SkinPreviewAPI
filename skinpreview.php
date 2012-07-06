a993e298907b40337f651caadf5c610c155c09a3
outadoc <outadoc@me.com>
2012-07-06 16:11:19 +0200

    Commented the code

--- skinpreview.php
+++ skinpreview.php
@@ -2,19 +2,38 @@
 	define('MC_SKINS_BASE_URL', 'http://s3.amazonaws.com/MinecraftSkins/');
 	$skin_src = 'char.png';
 	
-	if(isset($_GET['pseudo']) && $_GET['pseudo'] != null && !is404(MC_SKINS_BASE_URL . $_GET['pseudo'] . '.png')) {
+	//parameters: pseudo=[mc pseudo] OR url=[URL pointing to a skin png]
+	
+	if(isset($_GET['pseudo'])
+		&& $_GET['pseudo'] != null 
+		&& !is404(MC_SKINS_BASE_URL . $_GET['pseudo'] . '.png')
+	) {
+		//if pseudo is given and we can reach it (check using is404())
+		//we set the skin path to the url pointing to the pseudo on s3.amazonaws.com
 		$skin_src = MC_SKINS_BASE_URL . $_GET['pseudo'] . '.png';
-	} else if(isset($_GET['url']) && $_GET['url'] != null && !is404($_GET['url'])) {
+	} else if(isset($_GET['url']) 
+		&& $_GET['url'] != null 
+		&& !is404($_GET['url'])
+	) {
+		//else if we're given an URL and we can reach it
+		//we set the skin path to this url
 		$skin_src = $_GET['url'];
 	}
 	
+	//first, we load the skin
 	$skin = imagecreatefrompng($skin_src);
+	//then, we create the destination image (16*32 transparent png file)
 	$preview = imagecreatetruecolor(16, 32);
 	
+	//we want it to have a transparent background
 	$transparent = imagecolorallocatealpha($preview, 255, 255, 255, 127);
 	imagefill($preview, 0, 0, $transparent);
 	
+	
+	//if we want to get the preview of the back of the skin
 	if($_GET['side'] == 'back') {
+		//blitting the parts of the skin where they belong
+		
 		//head back
 		imagecopy($preview, $skin, 4, 0, 24, 8, 8, 8);
 		
@@ -23,7 +42,7 @@
 		
 		//arms back
 		imagecopy($preview, $skin, 0, 8, 52, 20, 4, 12);
-		flipSkin($preview, $skin, 12, 8, 52, 20, 4, 12);
+		flipSkin($preview, $skin, 12, 8, 52, 20, 4, 12); //flipSkin: in Minecraft, some parts of the skins are flipped horizontally. We're simulating it here.
 		
 		//legs back
 		imagecopy($preview, $skin, 4, 20, 12, 20, 4, 12);
@@ -31,7 +50,7 @@
 			
 		//armor
 		imagecopy($preview, $skin, 4, 0, 56, 8, 8, 8);
-	} else {
+	} else { //else, if we want the front of the skin
 		//face
 		imagecopy($preview, $skin, 4, 0, 8, 8, 8, 8);
 		
@@ -50,15 +69,18 @@
 		imagecopy($preview, $skin, 4, 0, 40, 8, 8, 8);
 	}
 	
-	imagedestroy($skin);
+	imagedestroy($skin); //we don't need this anymore
 	
-	$fullsize = imagecreatetruecolor(85, 170);
+	//resizing the preview: currently, it's a 16*32 file. We want it larger.
+	$fullsize = imagecreatetruecolor(85, 170); //85*170 is fine
 	imagesavealpha($fullsize, true);
 	$transparent = imagecolorallocatealpha($fullsize, 255, 255, 255, 127);
 	imagefill($fullsize, 0, 0, $transparent);
 	
+	//copying the preview to the full-sized image
 	imagecopyresized($fullsize, $preview, 0, 0, 0, 0, imagesx($fullsize), imagesy($fullsize), imagesx($preview), imagesy($preview));
 	
+	//aaaand we're done :D
 	header ("Content-type: image/png");
 	imagepng($fullsize);
 	
@@ -76,13 +98,13 @@
 	    }
 	}
 	
-	function flipSkin($preview, $skin, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h) {
+	function flipSkin($preview, $skin, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h) { //using the same sytax as imagecopy
 		$tmp = imagecreatetruecolor(4, 12);
 		imagecopy($tmp, $skin, 0, 0, $src_x, $src_y, $src_w, $src_h);
 		flipHorizontal($tmp);
 		imagecopy($preview, $tmp, $dst_x, $dst_y, 0, 0, $src_w, $src_h);
 		imagedestroy($tmp);
-	} 
+	}
 	
 	function flipHorizontal(&$img) {
 	 	$size_x = imagesx($img);
